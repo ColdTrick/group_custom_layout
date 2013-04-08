c2e2021e54cda49770fddca56762151cf52f9b7a
Jerome Bakker <jeabakker@coldtrick.com>
2013-04-08 10:22:59 +0200

    fixed: php error when custom background could not be found

--- procedures/get_background.php
+++ procedures/get_background.php
@@ -11,24 +11,29 @@
 				if(is_dir($dataroot . "group_custom_layout/backgrounds/")) {
 					$filename = $dataroot . "group_custom_layout/backgrounds/" . $layout->getOwnerGUID() . ".jpg";
 					
-					if($etag = md5(serialize(filemtime($filename)))) {
-						header("Etag: ". $etag);
-						$request_etag = $_SERVER["HTTP_IF_NONE_MATCH"];
-							
-						if(!empty($request_etag) && ($request_etag == $etag)) {
-							header("HTTP/1.0 304 Not Modified");
-			    			exit();
+					if (file_exists($filename)) {
+						if($etag = md5(serialize(filemtime($filename)))) {
+							header("Etag: ". $etag);
+							$request_etag = $_SERVER["HTTP_IF_NONE_MATCH"];
+								
+							if(!empty($request_etag) && ($request_etag == $etag)) {
+								header("HTTP/1.0 304 Not Modified");
+				    			exit();
+							}
 						}
-					}
-					
-					if($background = file_get_contents($filename)) {
-						header("Content-type: image");
-						header("Expires: " . date("r", time() + 864000));
-						header("Pragma: public");
-						header("Cache-Control: public");
-						header("Content-Length: " . strlen($background));
 						
-						echo $background;
+						if($background = file_get_contents($filename)) {
+							header("Content-type: image");
+							header("Expires: " . date("r", time() + 864000));
+							header("Pragma: public");
+							header("Cache-Control: public");
+							header("Content-Length: " . strlen($background));
+							
+							echo $background;
+						}
+					} else {
+						header("HTTP/1.0 404 Not Found");
+						exit();
 					}
 				}
 			}
