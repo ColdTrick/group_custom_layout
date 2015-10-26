c2e7084a2a60efbf81224ad6bc87b75b164e5fc1
Matt Beckett <beck24@gmail.com>
2015-10-25 22:59:12 -0700

    fix amd js

--- views/default/js/group_custom_layout.js
+++ views/default/js/group_custom_layout.js
@@ -2,19 +2,22 @@ define(['require', 'jquery', 'thickbox_js', 'farbtastic_js'], function(require, 
 
     if ($('#editForm').length) {
 
-        if ($('#enable_colors').val() != 'yes') {
-            $('#colorpicker_container').hide();
-        } else {
-            $('#colorpicker_container').show();
-        }
+        $(document).on('change', '#enable_colors', function() {
+            if ($('#enable_colors').val() != 'yes') {
+                $('#colorpicker_container').hide();
+            } else {
+                $('#colorpicker_container').show();
+            }
+        });
 
 
-
-        if ($('#enable_background').val() != 'yes') {
-            $('#background_container').hide();
-        } else {
-            $('#background_container').show();
-        }
+        $(document).on('change', '#enable_background', function() {
+            if ($('#enable_background').val() != 'yes') {
+                $('#background_container').hide();
+            } else {
+                $('#background_container').show();
+            }
+        });
 
 
         $('#backgroundpicker').farbtastic('#backgroundcolor');
